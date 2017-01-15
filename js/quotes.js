918da4fdcfbe72615d3d188289e586fd1060571b
Andrew Hickman <hickmaab@clarkson.edu>
2017-01-15 15:51:03 -0500

    Generate random quote with optional citation and year. Print to document.

---
+++ js/quotes.js
@@ -0,0 +1,26 @@
+//Define array of quotes accessible in the global scope
+
+let quotes = [
+	{
+		"quote": "Si me secas ut opinor effundiam colores stellarum vespertinarum.",
+		"source": "Urbs Strigum",
+		"citation": "Fortasse Somnio",
+		"year": "MMVIII",
+	},
+	{
+		"quote": "There is a crack in everything. That's how the light gets in.",
+		"source": "Leonard Cohen",
+		"citation": "Anthem, The Future",
+		"year": "1992" 
+	},
+	{
+		"quote": "Ashes and diamonds, foe and friend, we were all equal in the end.",
+		"source": "Pink Floyd",
+		"citation": "Two Suns in the Sunset, The Final Cut",
+		"year": "1983" 
+	},
+	{
+		"quote": "A very great vision is needed, and the man who has it must follow it as the eagle seeks the deepest blue of the sky.",
+		"source": "Crazy Horse, Oglala Lakota Sioux", 
+	}
+];
--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-// event listener to respond to "Show another quote" button clicks
+!(function(){
+
+	//Generate random number from length of quotes array, return random index from array with random number
+	function getRandomQuote(){
+		const randomNumber = Math.floor(Math.random() * quotes.length);
+		return quotes[randomNumber];
+	};
+
+	function printQuote(){
+		const quote = getRandomQuote();
+		const quoteBox = document.getElementById("quote-box");
+
+		//define markup for quote and source
+		let html = '<p class="quote">' + quote.quote + '</p>';
+		html += '<p class="source">' + quote.source;
+
+		//add citation if not omitted or empty
+		if (quote.citation !== undefined && quote.citation !== ""){
+			html += '<span class="citation">' + quote.citation + '</span>';
+		}
+
+		//add year if not omitted or empty
+		if (quote.year !== undefined && quote.year !== ""){
+			html += '<span class="year">' + quote.year + '</span>';
+		}
+
+		/*add tags if not omitted or empty
+		if (quote.tags !== undefined && quote.tags !== ""){
+			html += '<span class="tags">' + quote.tags + '</span>';
+		}*/
+		
+		//close source paragraph
+		html += '</p>';
+
+		quoteBox.innerHTML = html;
+	}
+
+	//Print quote on page load
+	printQuote();
+
+
+	// event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+
+}());
